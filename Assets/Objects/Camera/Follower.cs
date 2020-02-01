576986348fc753fed4189278336065bff0f5da99
Kamesuta <kamesuta@gmail.com>
2020-02-02 05:46:29 +0900

    壁

--- Assets/Objects/Camera/Follower.cs
+++ Assets/Objects/Camera/Follower.cs
@@ -8,12 +8,21 @@ public class Follower : MonoBehaviour
 {
     // プレイヤーのトランスフォーム
     public Transform target;
+    // 軸ロック
+    public Vector3 scale;
 
     void LateUpdate()
     {
         // ターゲットがあったら動作
         if (target != null)
+        {
             // 適用
-            transform.position = target.position;
+            var t = transform;
+            var position = t.position;
+            var diff = (target.position - position);
+            diff.Scale(scale);
+            position += diff;
+            t.position = position;
+        }
     }
 }
